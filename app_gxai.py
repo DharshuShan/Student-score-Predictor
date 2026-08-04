07378872eff169bc28221755fc43ea2cc2009cf4
DharshuShan <dharshudharsh00@gmail.com>
2026-08-04 12:40:24 +0200

    Update app_gxai.py

--- app_gxai.py
+++ app_gxai.py
@@ -18,6 +18,7 @@ import random
 from sklearn.inspection import permutation_importance, PartialDependenceDisplay
 warnings.filterwarnings('ignore')
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # ============================================================
 # PAGE CONFIGURATION
 # ============================================================
@@ -233,9 +234,9 @@ init_session_state()
 def load_model_and_scaler():
     """Load Model 2, scaler, and feature columns"""
     
-    model_path = '/Users/dharsh/Documents/Thesis/SHAP Model/final_exam_model1.pkl'
-    scaler_path = '/Users/dharsh/Documents/Thesis/SHAP Model/robust_scaler1.pkl'
-    columns_path = '/Users/dharsh/Documents/Thesis/SHAP Model/model_columns1.pkl'
+    model_path = os.path.join(BASE_DIR, "final_exam_model1.pkl")
+    scaler_path = os.path.join(BASE_DIR, "robust_scaler1.pkl")
+    columns_path = os.path.join(BASE_DIR, "model_columns1.pkl")
     
     if not os.path.exists(model_path):
         st.error(f"❌ Model not found at: {model_path}")
